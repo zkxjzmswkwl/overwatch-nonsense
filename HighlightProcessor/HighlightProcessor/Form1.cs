5c00b4369e3b2695a10a6b8a06d6bf6dc773bcdf
zkxjzmswkwl <smithcarterfreelance@gmail.com>
2021-11-04 08:19:23 -0600

    Highlight Processor

---
+++ HighlightProcessor/HighlightProcessor/Form1.cs
@@ -0,0 +1,142 @@
+﻿using FFMpegCore;
+using FFMpegCore.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HighlightProcessor
+{
+    struct Highlight
+    {
+        public TimeSpan Start;
+        public TimeSpan Delta;
+        public string Input;
+        public string Output;
+    }
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ProcessInput(Highlight h)
+        {
+            //-------------------------------------------------------------------------
+            // If operation ran, delete previous output before running new operation.
+            // Configure ffmpeg options to output as:
+            //          60      FPS
+            //          LibX264 Video Codec
+            //          AAC     Audio Codec
+            //          21      CRF
+            if (File.Exists(h.Output))
+                File.Delete(h.Output);
+
+
+            // ? this a bit much but I think is technically standard..?
+            FFMpegArguments
+                .FromFileInput(h.Input)
+                .OutputToFile(h.Output, false, options => options
+                    .Seek(h.Start)
+                    .WithFramerate(60)
+                    .WithDuration(h.Delta)
+                    .WithVideoCodec(VideoCodec.LibX264)
+                    .WithConstantRateFactor(21)
+                    .WithAudioCodec(AudioCodec.Aac)
+                    .WithVariableBitrate(4)
+                    .WithVideoFilters(filterOptions => filterOptions
+                        .Scale(VideoSize.Hd))
+                    .WithFastStart())
+                .ProcessSynchronously();
+
+            MessageBox.Show(h.Output, "Job's done.", MessageBoxButtons.OK);
+        }
+
+        private Highlight FetchTimes()
+        {
+
+            //-------------------------------------------------------------------------
+            // Set input to text box input, split to generate output path from input.
+            Highlight highlight = new Highlight();
+            highlight.Input = textBox1.Text;
+            highlight.Output = highlight.Input.Split(".")[0] + "_output.mp4";
+
+            if (textBox3.Text.Length > 1)
+                highlight.Start = TimeSpan.Parse(textBox3.Text);
+            if (textBox4.Text.Length > 1)
+                highlight.Delta = TimeSpan.Parse(textBox4.Text) - highlight.Start;
+
+            return highlight;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length <= 2 || textBox3.Text.Length <= 2 || textBox4.Text.Length <= 2)
+            {
+                MessageBox.Show("Fill out all inputs before processing.", "noob", MessageBoxButtons.OK);
+                return;
+            }
+
+            Highlight highlight = FetchTimes();
+            ProcessInput(highlight);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length <= 2)
+            {
+                MessageBox.Show("Must provide an input file for the intro/outro to be removed."
+                    , "noob"
+                    , MessageBoxButtons.OK);
+
+                return;
+            }
+
+            //-------------------------------------------------------------------------
+            // Overwatch highlights eat up 20 seconds with deadtime (intro/outro).
+            // This removes both of those (6s intro, 14s outro). 
+            Highlight highlight = FetchTimes();
+            var FileInfo = FFProbe.Analyse(highlight.Input);
+            highlight.Start = TimeSpan.Parse("00:00:06");
+            highlight.Delta = FileInfo.Duration - TimeSpan.Parse("00:00:14");
+
+            ProcessInput(highlight);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Author: Carter\nDate: May 2018\nRevision Date: Nov. 2021", "About", MessageBoxButtons.OK); 
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var DialogR = openFileDialog1.ShowDialog();
+            if (DialogR == DialogResult.OK)
+            {
+                var FileName = openFileDialog1.FileName;
+                try
+                {
+                    textBox1.Text = System.IO.Path.GetFullPath(FileName);
+                }
+                catch
+                {
+                    MessageBox.Show("Couldn't find the file path. Try entering it manually.");
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox3.Text = "00:00:05";
+            textBox4.Text = "00:00:25";
+        }
+    }
+}
---
+++ HighlightProcessor/README.md
@@ -0,0 +1,16 @@
+# Overwatch highlight processor
+This project processes Overwatch highlights based on user input.
+
+![img](https://i.imgur.com/uO8iiKf.png)
+
+"Remove highlight intro/outro" button will remove the obnoxious 6 second intro and 14 second outro from your Overwatch highlight, then output the file to `file_name_output.mp4` in the same directory.
+
+`start` and `end` are `hh:mm:ss` formatted. This is `hourhour:minuteminute:secondsecond`.
+
+If you want your highlight to start at 5 seconds and end at 16 seconds, you would set them as follows:
+
+Start -> `00:00:05`
+
+End   -> `00:00:16`
+
+A download of this is available [here](https://mega.nz/file/E9sU1ZDK#AdS9iIAQEcXCHA6mP6DNLjE-Sn8ghCxHDGkoIpfdLyY)
